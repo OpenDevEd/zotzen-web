b7a96bb14df1b5b3c48864e7c6889ffe6ff6163e
ZainaSaadeddin <zeina.sadal.deen@hotmail.com>
2020-12-31 14:50:03 +0200

    app setup

--- api/app.js
+++ api/app.js
@@ -7,7 +7,11 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var testAPIRouter = require("./routes/testAPI");
+
+
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use("/testAPI", testAPIRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
