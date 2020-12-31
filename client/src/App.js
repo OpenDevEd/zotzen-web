5ee7692c63129b76a9c9e566552234f2acdfa53b
ZainaSaadeddin <zeina.sadal.deen@hotmail.com>
2020-12-31 15:07:13 +0200

    Connecting the React Client to the Express API

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-     <h2>The Login Component</h2>
-    </div>
-  );
+
+
+class App extends Component{
+
+
+
+constructor(props) {
+  super(props);
+  this.state = { apiResponse: "" };
+}
+
+callAPI() {
+  fetch("http://localhost:9000/testAPI")
+      .then(res => res.text())
+      .then(res => this.setState({ apiResponse: res }));
+}
+
+componentWillMount() {
+  this.callAPI();
+}
+
+render(){
+      return(
+      <div>
+      <h3>welcome to React</h3>
+      <p className="App-intro">;{this.state.apiResponse}</p>
+      </div>
+      );
+  }
 }
 
 export default App;
