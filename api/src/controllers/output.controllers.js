caac3b07f8893ed3061c5995799549cec9566abf
Jacques Nyilinkindi <jacquesnyilinkindi@jacquesnyilinkindi.local>
2021-01-19 16:22:36 +0200

    Fetched output categories

--- api/src/controllers/output.controllers.js
+++ api/src/controllers/output.controllers.js
@@ -1,10 +1,18 @@
-//import {zoteroLib} from 'zotero-lib';
+import zoterolib from 'zotero-lib';
+import { config } from 'dotenv';
+import { filterOutputCategories } from '../helpers/filterOutputCategories';
 
-
+config();
+const { OUTPUT_CATEGORY_KEY } = process.env;
+const zoterolibIns = new zoterolib();
 
 export const listCategories = async (req, res) => {
   try {
-    //let categories = await zoteroLib.collections(2405685, 'MP53N7NC');
+    let categories = await zoterolibIns.collections({
+      key: OUTPUT_CATEGORY_KEY,
+      terse: true
+    });
+    categories = await filterOutputCategories(categories);
     return res.status(200).json({ categories, statusCode: 200 });
   } catch (error) {
     const { message, status = 400 } = error;
--- api/src/routes/output.routes.js
+++ api/src/routes/output.routes.js
@@ -1,15 +1,15 @@
 import { Router } from 'express';
 import * as controller from '../controllers/output.controllers';
-import { verifyToken } from '../middleware/auth.middleware';
+import { verifyToken as authenticateUser } from '../middleware/auth.middleware';
 const routes = Router();
 
 // Listing categories
-routes.get('/categories', verifyToken, controller.listCategories);
+routes.get('/categories', authenticateUser, controller.listCategories);
 
 // Listing my records
-routes.get('/', verifyToken, controller.test);
+routes.get('/', authenticateUser, controller.test);
 
 // Creating new record
-routes.post('/', verifyToken, controller.test);
+routes.post('/', authenticateUser, controller.test);
 
 export default routes;
