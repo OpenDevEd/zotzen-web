fb5317108944ff9b93bb137a8ae042a6c88542af
ZainaSaadeddin <zeina.sadal.deen@hotmail.com>
2021-03-14 20:19:34 +0200

    env changes

--- api/src/helpers/zotzen.js
+++ api/src/helpers/zotzen.js
@@ -3,16 +3,17 @@ import { config } from "dotenv";
 
 config();
 
-const { OUTPUT_GROUP_ID, ZOTERO_API_KEY, ZENODO_ACCESS_TOKEN } = process.env;
+const { OUTPUT_GROUP_ID, ZOTERO_API_KEY, ZENODO_ACCESS_TOKEN, ZENODO_SANDBOX, KERKO_URL } = process.env;
 
 const defaultOutputData = {
   verbose: true,
   zotero_api_key: ZOTERO_API_KEY,
   zenodo_access_token: ZENODO_ACCESS_TOKEN,
+  zenodo_sandbox: ZENODO_SANDBOX,
   institution: "EdTech Hub",
   language: "en",
-  rights: "Creative Commons Attribution 4.0",
-  kerko_url: "https://docs.edtechhub.org/lib/",
+  rights: "Creative Commons Attribution 4.0 International", 
+  kerko_url: KERKO_URL,
   tags: ["_r:AddedByZotZen"],
   description: "An output of the EdTech Hub, https://edtechhub.org",
 };
